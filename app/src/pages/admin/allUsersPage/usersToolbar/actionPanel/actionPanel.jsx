715a48bce76e321e4f09fedbcafc6ae570550157
Aleksandra Ivleva <Aleksandra_Ivleva@epam.com>
2019-02-25 16:15:20 +0300

    1commit

--- app/src/pages/admin/allUsersPage/usersToolbar/actionPanel/actionPanel.jsx
+++ app/src/pages/admin/allUsersPage/usersToolbar/actionPanel/actionPanel.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { injectIntl, defineMessages, intlShape } from 'react-intl';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { GhostButton } from 'components/buttons/ghostButton';
 import ExportIcon from 'common/img/export-inline.svg';
 import InviteUserIcon from 'common/img/ic_invite-inline.svg';
 import AddUserIcon from 'common/img/ic-add-user-inline.svg';
+import { allUsersSelector } from 'controllers/administrate/allUsers';
+import { showNotification, NOTIFICATION_TYPES } from 'controllers/notification';
+import { showModalAction } from 'controllers/modal';
 import { EXPORT, INVITE_USER, ADD_USER } from './constants';
 import styles from './actionPanel.scss';
 
@@ -23,40 +28,64 @@ const messages = defineMessages({
     id: 'AllUsersPage.AddUser',
     defaultMessage: 'Add user',
   },
+  addUserNotification: {
+    id: 'ActionPanel.addUserNotification',
+    defaultMessage: 'Member was successfully created',
+  },
 });
 
-const ACTION_BUTTONS = [
+@connect(
+  (state) => ({
+    users: allUsersSelector(state),
+  }),
   {
-    key: EXPORT,
-    icon: ExportIcon,
-    grayBorder: true,
+    showModal: showModalAction,
+    showNotification,
   },
-  {
-    key: INVITE_USER,
-    icon: InviteUserIcon,
-    grayBorder: false,
-  },
-  {
-    key: ADD_USER,
-    icon: AddUserIcon,
-    grayBorder: false,
-  },
-];
-
+)
 @injectIntl
 export class ActionPanel extends Component {
   static propTypes = {
     intl: intlShape.isRequired,
+    showNotification: PropTypes.func,
+  };
+  static defaultProps = {
+    showNotification: () => {},
+  };
+  onAddUser = () => {
+    const { intl } = this.props;
+    this.props.showNotification({
+      type: NOTIFICATION_TYPES.SUCCESS,
+      message: intl.formatMessage(messages.addUserNotification),
+    });
   };
   renderHeaderButtons = () => {
     const {
       intl: { formatMessage },
     } = this.props;
+    const ACTION_BUTTONS = [
+      {
+        key: EXPORT,
+        icon: ExportIcon,
+        grayBorder: true,
+      },
+      {
+        key: INVITE_USER,
+        icon: InviteUserIcon,
+        grayBorder: false,
+      },
+      {
+        key: ADD_USER,
+        icon: AddUserIcon,
+        grayBorder: false,
+        onClick: this.onAddUser,
+      },
+    ];
     return (
       <div className={cx('action-buttons')}>
-        {ACTION_BUTTONS.map(({ key, icon, grayBorder }) => (
+        {ACTION_BUTTONS.map(({ key, icon, grayBorder, onClick }) => (
           <div className={cx('action-button')} key={key}>
-            <GhostButton icon={icon} grayBorder={grayBorder}>
+            <GhostButton icon={icon} grayBorder={grayBorder} onClick={onClick}>
               {formatMessage(messages[key])}
             </GhostButton>
           </div>
